5da273d047b77f7423c3a4ffcee49cc9675306ff
Jakobe16 <jakostom20@gmail.com>
2021-12-16 13:37:15 -0600

    add missing files

---
+++ Tuesday/ContentView.swift
@@ -0,0 +1,178 @@
+//
+//  ContentView.swift
+//  Tuesday
+//
+//  Created by PM JavaScript - Ja'kobe Stumon on 10/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   @State private var playerCard = "back"
+   @State private var cpuCard = "back"
+   @State private var playerScore = 0
+   @State private var cpuScore = 0
+    
+    
+    var body: some View {
+       
+        
+        
+        ZStack {
+
+                
+
+                    
+
+                    Image("background").ignoresSafeArea()
+
+                    
+
+            VStack {
+
+                    
+
+                        Spacer()
+
+                        Image("logo")
+
+                        Spacer()
+
+                        HStack {
+
+                        
+
+                            Spacer()
+
+                            Image(playerCard)
+
+                            Spacer()
+
+                            Image(cpuCard)
+
+                            Spacer()
+
+                        
+
+                            
+
+                            
+
+                        }
+
+                        Spacer()
+                
+                Button(action: {
+                    // Generate a random numbers between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+
+                    // Update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    // Update the score
+                    if playerRand > cpuRand{
+                        playerScore += 1
+                        }
+                    else if playerRand == cpuRand{
+                        playerScore += 0
+                        cpuScore += 0
+                    }
+                    
+                    else {
+                        cpuScore += 1
+                    }
+                    
+                    //cpuScore += 1
+                    
+                }, label: {
+                    
+                    Image("dealbutton")
+                })
+                       
+
+                        Spacer()
+
+                        HStack {
+
+                        
+
+                            Spacer()
+
+                            VStack {
+
+                            
+
+                                
+
+                                Text("Player")
+
+                                    .font(.headline)
+
+                                    .foregroundColor(Color.white)
+
+                                    .padding(.bottom, 10.0)
+
+                                Text(String(playerScore) )
+
+                                    .font(.largeTitle)
+
+                                    .foregroundColor(Color.white)
+
+                                
+
+                            }
+
+                            Spacer()
+
+                            VStack {
+
+                            
+
+                                
+
+                                Text("CPU")
+
+                                    .font(.headline)
+
+                                    .foregroundColor(Color.white)
+
+                                    .padding(.bottom, 10.0)
+
+                                Text(String(cpuScore))
+
+                                    .font(.largeTitle)
+
+                                    .foregroundColor(Color.white)
+
+                                
+
+                            }
+
+                            Spacer()
+
+                        }
+
+                        Spacer()
+
+                    }
+
+                        
+
+                    
+
+                    
+
+            
+
+                }
+        
+        
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tuesday/TuesdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuesdayApp.swift
+//  Tuesday
+//
+//  Created by PM JavaScript - Ja'kobe Stumon on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct TuesdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
